1d8e1d1e7fccc24ff757657a7571f9e44bbf682b
josecazan <170300768@ucaribe.edu.mx>
2021-03-21 13:18:28 -0500

    Creacion de los modelos para carros y usuarios -- Angel

---
+++ AngularKavak/back-end/back-end/Models/Cars.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Models
+{
+   public class Cars
+   {
+      [Key]
+      public int Id { get; set; }
+
+      [Required]
+      public string Name { get; set; }
+
+      [Required]
+      public string Model { get; set; }
+
+      [Required]
+      public string Brand { get; set; }
+
+      [Required]
+      public int Year { get; set; }
+
+      [Required]
+      public string Type { get; set; }
+
+      [Required]
+      public int Mileage { get; set; }
+
+      [Required]
+      [Column(TypeName = "decimal(18,4)")]
+      public decimal Price { get; set; }
+
+      [Required]
+      public string City { get; set; }
+   }
+}
