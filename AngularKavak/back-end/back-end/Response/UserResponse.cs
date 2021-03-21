955bb5d6c905825ebec48426c8a988f1eb033e39
josecazan <170300768@ucaribe.edu.mx>
2021-03-21 13:25:35 -0500

    Modelo de respuesta JWT -- Angel

---
+++ AngularKavak/back-end/back-end/Response/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Response
+{
+   public class UserResponse
+   {
+      public string Email { get; set; }
+
+      public string Token { get; set; }
+
+      public string Role { get; set; }
+   }
+}
