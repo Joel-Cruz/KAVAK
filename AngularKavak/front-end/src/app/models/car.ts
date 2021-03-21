6202854c2d4b0b761dfca1ae3b13a65d22abb101
josecazan <170300768@ucaribe.edu.mx>
2021-03-21 14:38:40 -0500

    Implementacion de los modelos cars y users -- Mauricio

---
+++ AngularKavak/front-end/src/app/models/car.ts
@@ -0,0 +1,11 @@
+export class Car {
+    id?: number;
+    name: string;
+    model: string;
+    brand: string;
+    year: number;
+    type: string;
+    mileage: number;
+    city: string;
+    price: number;
+}
