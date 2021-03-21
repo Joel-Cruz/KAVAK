c2345700b269a23f3f69f45320db982feb7b88a6
josecazan <170300768@ucaribe.edu.mx>
2021-03-21 13:24:35 -0500

    Clase para crear las migraciones a base de datos -- Aldecua

---
+++ AngularKavak/back-end/back-end/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using back_end.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end
+{
+   public partial class ApplicationDbContext : DbContext
+   {
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseSqlServer("Data Source=DESKTOP-V7D0MEI;Database=KavakDB;Integrated Security=true");
+      }
+
+
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+      {
+
+      }
+
+      public DbSet<Cars> Cars { get; set; }
+
+      public DbSet<Users> Users { get; set; }
+
+   }
+}
